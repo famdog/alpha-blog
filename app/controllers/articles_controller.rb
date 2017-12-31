d86c17551573ba6fa2eb2208d8dc6f2898d35165
famdog <famdogdev@gmail.com>
2017-12-31 03:44:11 +0000

    made the app

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+  
+  def index 
+    @articles = Article.all  
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def new
+      @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def create 
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to article_path(@article)
+    else 
+      render 'new'
+    end
+    
+  end
+  
+  def update 
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article updated successfully"
+      redirect_to article_path(@article)
+    else 
+      render 'edit'
+    end
+  end 
+  
+  def destroy 
+    @article = Article.find(params[:id])
+    if @article.destroy
+      flash[:notice] = "Article was deleted successfully"
+      redirect_to articles_path
+    else 
+      render 'index'
+    end
+  end
+    
+  
+  private
+    def article_params 
+      params.require(:article).permit(:title, :description)
+    end
+    
+end
